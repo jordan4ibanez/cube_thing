4c3eec36f56e6c6bce06f5b4b1fc74455dec9246
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-03 21:16:22 -0500

    A very basic app

--- source/app.d
+++ source/app.d
@@ -1,13 +1,34 @@
 import std.stdio;
 
 import graphics.camera_handler;
+import graphics.font_handler;
 import raylib;
+import utility.window;
 
 void main() {
 
+	scope (exit) {
+		// FontHandler.terminate();
+		// ShaderHandler.terminate();
+		// TextureHandler.terminate();
+		// ModelHandler.terminate();
+		Window.terminate();
+		CameraHandler.terminate();
+	}
+
+	validateRaylibBinding();
+
+	SetTraceLogLevel(TraceLogLevel.LOG_WARNING);
+
+	Window.initialize();
+
 	CameraHandler.initialize();
 
-	// InitWindow(0,0)
+	while (Window.shouldStayOpen()) {
+		BeginDrawing();
 
-	CloseWindow();
+		ClearBackground(Colors.GRAY);
+
+		EndDrawing();
+	}
 }
