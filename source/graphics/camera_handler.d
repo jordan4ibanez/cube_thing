dd0e1deba958c617a9da3ce1433c5cbc335e1baa
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-04 10:39:56 -0500

    Update camera_handler.d

--- source/graphics/camera_handler.d
+++ source/graphics/camera_handler.d
@@ -1,6 +1,7 @@
 module graphics.camera_handler;
 
 import raylib;
+import utility.window;
 
 static final const class CameraHandler {
 static:
@@ -12,10 +13,35 @@ private:
 
     public void initialize() {
         camera = new Camera2D();
+        camera.rotation = 0;
     }
 
     public void terminate() {
         camera = null;
+    }
+
+    public void begin() {
+        BeginMode2D(*camera);
+    }
+
+    public void end() {
+        EndMode2D();
+    }
+
+    public void setTarget(Vector2 position) {
+        camera.target = position;
+    }
+
+    public float getZoom() {
+        return camera.zoom;
+    }
+
+    public void setZoom(float zoom) {
+        camera.zoom = zoom;
+    }
+
+    public void __update() {
+        camera.offset = Vector2Multiply(Window.getSize(), Vector2(0.5, 0.5));
     }
 
     //* BEGIN INTERNAL API.
