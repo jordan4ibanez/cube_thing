ed6dfddfae94ab49c727f553a3ea04e2438b517a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-05 16:55:00 -0500

    Create some data

---
+++ source/graphics/map.d
@@ -0,0 +1,16 @@
+module graphics.map;
+
+import raylib;
+
+immutable int CHUNK_WIDTH = 32;
+immutable int CHUNK_HEIGHT = 128;
+
+class Chunk {
+    int[CHUNK_WIDTH][CHUNK_HEIGHT] data;
+}
+
+static final const class Map {
+static:
+private:
+
+}
