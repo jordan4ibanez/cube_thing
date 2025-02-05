9ad2827f43ad17a923e32da4b577273854a614bb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-04 22:56:04 -0500

    Create mouse.d

---
+++ source/controls/mouse.d
@@ -0,0 +1,27 @@
+module controls.mouse;
+
+import raylib;
+
+static final const class Mouse {
+static:
+private:
+
+    //? I like to have specific modules for things.
+
+    //* BEGIN PUBLIC API.
+
+    public Vector2 getDelta() {
+        return GetMouseDelta();
+    }
+
+    public bool isButtonPressed(MouseButton button) {
+        return IsMouseButtonPressed(button);
+    }
+
+    public bool isButtonDown(MouseButton button) {
+        return IsMouseButtonDown(button);
+    }
+
+    //* BEGIN INTERNAL API.
+
+}
