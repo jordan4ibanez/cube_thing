e7cbaf9671a501cfa6428522e42a70e26b821edf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-05 16:46:50 -0500

    Standardize an inversion function

--- source/game/player.d
+++ source/game/player.d
@@ -66,6 +66,10 @@ private:
         DrawRectangleV(centerCollisionboxBottom(position, size), size, Colors.WHITE);
     }
 
+    public Vector2 getPositionInWorldSpace() {
+        return Vector2(position.x, -position.y);
+    }
+
     public void move() {
         double delta = Delta.getDelta();
         Vector2 playerPos = getPosition();
