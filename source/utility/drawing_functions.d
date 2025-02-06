9df44aa94d943fa4efe0ff0579451d5ebb71a89d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-06 14:47:31 -0500

    Make this less sloppy

--- source/utility/drawing_functions.d
+++ source/utility/drawing_functions.d
@@ -4,7 +4,8 @@ import raylib;
 
 /// This takes the game coordinates and shifts it to be drawn by raylib for a rectangle.
 Vector2 centerCollisionboxBottom(Vector2 position, Vector2 size) {
-    Vector2 offset = Vector2Multiply(size, Vector2(0.5, 1));
-    Vector2 rectangleOrigin = Vector2Subtract(position, offset);
-    return rectangleOrigin;
+    Vector2 newPosition = position;
+    newPosition.x -= size.x * 0.5;
+    newPosition.y += size.y;
+    return newPosition;
 }
