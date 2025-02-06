0d8a60be1199bfd2a2d9cfba541b7a5be05724fd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-06 14:28:36 -0500

    It's custom time

--- source/graphics/render.d
+++ source/graphics/render.d
@@ -1,11 +1,21 @@
 module graphics.render;
 
+import raylib;
+
 static final const class Render {
 static:
 private:
 
 public: //* BEGIN PUBLIC API.
 
+    void rectangle(Vector2 position, Vector2 rotation, Color color) {
+        DrawRectangleV(invertPosition(position), rotation, color);
+    }
+
 private: //* BEGIN INTERNAL API.
+
+    Vector2 invertPosition(Vector2 position) {
+        return Vector2(position.x, -position.y);
+    }
 
 }
