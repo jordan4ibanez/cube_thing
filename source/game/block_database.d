4eee10a2035dccd58ed30b3d8415f2fabff95693
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-07 15:31:21 -0500

    More data

--- source/game/block_database.d
+++ source/game/block_database.d
@@ -1,14 +1,43 @@
 module game.block_database;
 
-
+class BlockDefinition {
+    string name = null;
+    string modName = null;
+    string texture = null;
+    int id = 0;
+}
 
 static final const class BlockDatabase {
 static:
 private:
 
-    // Chunk[int] database;
+    BlockDefinition[string] databaseName;
+    BlockDefinition[int] databaseID;
 
 public: //* BEGIN PUBLIC API.
+
+    void registerBlock(BlockDefinition newBlock) {
+
+        if (newBlock.name is null) {
+            throw new Error("Name for block is null.");
+        }
+
+        if (newBlock.name in databaseName) {
+            throw new Error("Trying to override block " ~ newBlock.name);
+        }
+
+        if (newBlock.modName is null) {
+            throw new Error("Mod name for block is null.");
+        }
+
+        if (newBlock.texture is null) {
+            throw new Error("Texture for block is null.");
+        }
+
+        if (newBlock.id <= 0) {
+
+        }
+    }
 
 private: //* BEGIN INTERNAL API.
 
