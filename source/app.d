8fa9f414b4121a3d00e1259762a256caf45cbef3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-06 19:26:16 -0500

    Data

--- source/app.d
+++ source/app.d
@@ -30,17 +30,20 @@ void main() {
 
 	Window.initialize();
 
+	// SetWindowState(ConfigFlags.FLAG_VSYNC_HINT);
+	// SetTargetFPS(100);
+
 	CameraHandler.initialize();
 
-	Map.initialize();
+	Map.loadChunk(0);
 
 	while (Window.shouldStayOpen()) {
-
-		CameraHandler.centerToPlayer();
 
 		double delta = Delta.getDelta();
 
 		Player.move();
+
+		CameraHandler.centerToPlayer();
 
 		BeginDrawing();
 		{
@@ -58,10 +61,16 @@ void main() {
 			Vector2 center = Vector2Multiply(Window.getSize(), Vector2(0.5, 0.5));
 			DrawCircle(cast(int) center.x, cast(int) center.y, 4, Colors.RED);
 
+			DrawText(toStringz("fps:" ~ to!string(GetFPS())), 0, 0, 120, Colors.WHITE);
 			Vector2 playerPos = Player.getPosition();
-			DrawText(toStringz("y: " ~ to!string(playerPos.y)), 0, 0, 120, Colors.WHITE);
-			DrawText(toStringz("x: " ~ to!string(playerPos.x)), 0, 120, 120, Colors.WHITE);
+			DrawText(toStringz("y: " ~ to!string(playerPos.y)), 0, 120, 120, Colors.WHITE);
+			DrawText(toStringz("x: " ~ to!string(playerPos.x)), 0, 240, 120, Colors.WHITE);
+
+			int inChunk = Player.inWhichChunk();
+			DrawText(toStringz("chunk:" ~ to!string(inChunk)), 0, 360, 120, Colors.WHITE);
+
 		}
 		EndDrawing();
+
 	}
 }
