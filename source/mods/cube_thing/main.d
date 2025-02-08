f25a1600a12cdb87e0e39349d1d18657333ad357
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-07 23:46:45 -0500

    stone and grass

--- source/mods/cube_thing/main.d
+++ source/mods/cube_thing/main.d
@@ -10,10 +10,19 @@ class CubeThingBlock : BlockDefinition {
 
 void cubeThingMain() {
 
+    CubeThingBlock stone = new CubeThingBlock();
+    stone.name = "stone";
+    stone.texture = "default_stone.png";
+    BlockDatabase.registerBlock(stone);
+
     CubeThingBlock dirt = new CubeThingBlock();
     dirt.name = "dirt";
     dirt.texture = "default_dirt.png";
-
     BlockDatabase.registerBlock(dirt);
+
+    CubeThingBlock grass = new CubeThingBlock();
+    grass.name = "grass";
+    grass.texture = "default_grass.png";
+    BlockDatabase.registerBlock(grass);
 
 }
