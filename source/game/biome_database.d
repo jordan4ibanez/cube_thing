6c9eba45a05a82d39c8fbc2fc64e06570b7347ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-07 23:49:04 -0500

    ID dispatcher

--- source/game/biome_database.d
+++ source/game/biome_database.d
@@ -2,6 +2,7 @@ module game.biome_database;
 
 class BiomeDefinition {
     string name = null;
+    string modName = null;
 
     string grassLayer = null;
     int grassLayerID = -1;
@@ -19,6 +20,8 @@ private:
     BiomeDefinition[string] nameDatabase;
     BiomeDefinition[int] idDatabase;
 
+    int currentID = 0;
+
 public: //* BEGIN PUBLIC API.
 
     void registerBiome(BiomeDefinition newBiome) {
@@ -29,6 +32,10 @@ public: //* BEGIN PUBLIC API.
 
         if (newBiome.name in nameDatabase) {
             throw new Error("Tried to overwrite biome" ~ newBiome.name);
+        }
+
+        if (newBiome.modName is null) {
+            throw new Error("Mod name is missing from biome " ~ newBiome.name);
         }
 
         if (newBiome.grassLayer is null) {
@@ -51,5 +58,15 @@ public: //* BEGIN PUBLIC API.
     }
 
 private: //* BEGIN INTERNAL API.
+
+    // todo: make this pull the standard IDs into an associative array from the mongoDB.
+    // todo: mongoDB should store the MAX current ID and restore it.
+    // todo: Then, match to it. If it doesn't match, this is a new block.
+    // todo: Then you'd call into this. :)
+    int nextID() {
+        int thisID = currentID;
+        currentID++;
+        return thisID;
+    }
 
 }
