d3e17c3d4c1e1471431ff8dc31bd61cab2a84669
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-07 22:50:25 -0500

    More framework

--- source/game/biome_database.d
+++ source/game/biome_database.d
@@ -1,6 +1,7 @@
 module game.biome_database;
 
 class BiomeDefinition {
+    string name = null;
 
     string grassLayer = null;
     int grassLayerID = -1;
@@ -14,16 +15,32 @@ static final const class BlockDatabase {
 static:
 private:
 
+    // Faster access based on ID or name.
+    BiomeDefinition[string] nameDatabase;
+    BiomeDefinition[int] idDatabase;
+
 public: //* BEGIN PUBLIC API.
 
     void registerBiome(BiomeDefinition newBiome) {
 
-        if (newBiome.grassLayer is null) {
+        if (newBiome.name is null) {
+            throw new Error("Biome is missing a name.");
+        }
 
+        if (newBiome.name in nameDatabase) {
+            throw new Error("Tried to overwrite biome" ~ newBiome.name);
+        }
+
+        if (newBiome.grassLayer is null) {
+            throw new Error("Grass layer missing from biome " ~ newBiome.name);
         }
 
         if (newBiome.dirtLayer is null) {
+            throw new Error("Dirt layer missing from biome " ~ newBiome.name);
+        }
 
+        if (newBiome.stoneLayer is null) {
+            throw new Error("Stone layer missing from biome " ~ newBiome.name);
         }
 
     }
