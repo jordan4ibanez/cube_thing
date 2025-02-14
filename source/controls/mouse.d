b024a7333c486bdb16507c6e66276d8fbeed78ad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-14 18:19:28 -0500

    Mouse overhaul

--- source/controls/mouse.d
+++ source/controls/mouse.d
@@ -1,25 +1,37 @@
 module controls.mouse;
 
 public import raylib : MouseButton;
+import graphics.camera_handler;
+import math.vec2d;
 import raylib;
+import utility.window;
 
 static final const class Mouse {
 static:
 private:
 
+public:
+
     //? I like to have specific modules for things.
 
     //* BEGIN PUBLIC API.
 
-    public Vector2 getDelta() {
-        return GetMouseDelta();
+    Vec2d getDelta() {
+        return Vec2d(GetMouseDelta());
     }
 
-    public bool isButtonPressed(MouseButton button) {
+    Vec2d getWorldPosition() {
+        Vec2d mPosition = Vec2d(GetMousePosition());
+        immutable int windowHeight = Window.getHeight();
+        mPosition.y = windowHeight - mPosition.y;
+        return CameraHandler.screenToWorld(mPosition);
+    }
+
+    bool isButtonPressed(MouseButton button) {
         return IsMouseButtonPressed(button);
     }
 
-    public bool isButtonDown(MouseButton button) {
+    bool isButtonDown(MouseButton button) {
         return IsMouseButtonDown(button);
     }
 
